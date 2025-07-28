b35e255cc5a64581b2007606de866094b1d0d0ba
Mert <espawnn@gmail.com>
2025-07-28 18:06:27 +0300

    Project v0.33

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,18 @@
 import HeaderMenu from "./headerDropMenu.js";
 import MainPage from "./mainPage.js";
+import { Routes, Route } from "react-router-dom";
+import AidatGecikme from "./pages/diger/aidatGecikme.js";
+import AracMuayene from "./pages/diger/aracMuayene.js";
+import Burc from "./pages/diger/burc.js";
+import KelimeSayisi from "./pages/diger/kelimeSayisi.js";
+import DersNotu from "./pages/egitim/dersNotu.js";
+import EokulNot from "./pages/egitim/eokulNot.js";
+import LiseMezuniyet from "./pages/egitim/liseMezuniyet.js";
+import LiseOrtalama from "./pages/egitim/liseOrtalama.js";
+import LiseSinifGecme from "./pages/egitim/liseSinifGecme.js";
+import TakdirTesekkur from "./pages/egitim/takdirTesekkur.js";
+import UniversiteNotOrtalama from "./pages/egitim/universiteNotOrtalama.js";
+import VizeFinalOrtalama from "./pages/egitim/vizeFinalOrtalama.js";
 
 
 function App () {
@@ -10,7 +23,21 @@ function App () {
           <p className="header-title">Hesaplar</p>
           <HeaderMenu />
         </header>
-          <MainPage />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/aidatGecikme" element={<AidatGecikme />} />
+          <Route path="/aracmuayene" element={<AracMuayene />} />
+          <Route path="/burc" element={<Burc />} />
+          <Route path="/kelimesayisi" element={<KelimeSayisi />} />
+          <Route path="/dersnotu" element={<DersNotu />} />
+          <Route path="/eokulnot" element={<EokulNot />} />
+          <Route path="/lisemezuniyet" element={<LiseMezuniyet />} />
+          <Route path="/liseortalama" element={<LiseOrtalama />} />
+          <Route path="/lisesinifgecme" element={<LiseSinifGecme />} />
+          <Route path="/takdirtesekkur" element={<TakdirTesekkur />} />
+          <Route path="/universitenotortalama" element={<UniversiteNotOrtalama />} />
+          <Route path="/vizefinalortalama" element={<VizeFinalOrtalama />} />
+        </Routes>
       </div>
     </div>
   );
---
+++ src/components/pages/egitim/eokulNot.js
@@ -0,0 +1,13 @@
+
+
+function EokulNot() {
+  return (
+    <div className="eokul-not">
+      <h1>E-Okul Not Hesaplama</h1>
+      <p>Bu sayfa e-okul not hesaplama işlemleri için kullanılacaktır.</p>
+      {/* E-okul not hesaplama formu ve işlemleri buraya eklenebilir */}
+    </div>
+  );
+}
+
+export default EokulNot;
